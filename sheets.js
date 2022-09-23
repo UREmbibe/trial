af89be212add690ec979c518b5e9ba97d82385ac
UREmbibe <114053472+UREmbibe@users.noreply.github.com>
2022-09-23 14:38:54 +0530

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,24 +1,27 @@
 const sheetID = "146bjpxiyAd-5KlFj5Q8K0O7PWNxSElv1l_I2EF6vdkI";
-const base = "https://docs.google.com/spreadsheets/d/"+sheetID+"/gviz/tq?";
+const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
 
 const sheetName = "users";
-const query = encodeURIComponent('Select *');
-const url = '${base}&sheet=${sheetName}&tq=${query}'
-const data = []
+const query = encodeURIComponent("Select *");
+const url = `${base}&sheet=${sheetName}&tq=${query}`
+console.log(url)
+const data = [];
 
-document.addEventListener('DOMContentLoaded', loadDoc)
+document.addEventListener('DOMContentLoaded', init)
 
-/*function init(){
-    console.log("ready")
+function init(){
+    console.log("ready");
     fetch(url)
-    .then(res.text())
+    .then(res => res.text())
     .then(rep => {
         console.log(rep);
-        const jso = JSON.parse(rep.substr(47).slice(0,-2));
-        console.log(jso)
+        //console.log(res);
+        //const js0 = JSON.parse(rep.substr(47).slice(0,-2));
+        //console.log(jso)
     })
-}*/
-function loadDoc() {
+}
+
+/*function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -27,4 +30,4 @@ function loadDoc() {
   };
   xhttp.open("GET", url, true);
   xhttp.send();
-}
+}*/
